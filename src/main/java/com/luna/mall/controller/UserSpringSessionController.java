04367d1f0e4694daa79f524ea1f5a61dc28ce08f
luna <yljloveliucan@126.com>
2018-09-01 22:26:31 +0800

    spring session redis

---
+++ src/main/java/com/luna/mall/controller/UserSpringSessionController.java
@@ -0,0 +1,48 @@
+package com.luna.mall.controller;
+
+import com.luna.mall.common.Constants;
+import com.luna.mall.common.ServerResponse;
+import com.luna.mall.pojo.User;
+import com.luna.mall.serivce.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/springSession")
+public class UserSpringSessionController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = userService.login(username,password);
+        if(response.isSuccess()){
+            session.setAttribute(Constants.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        session.removeAttribute(Constants.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "getUserInfo.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session){
+        User user = (User)session.getAttribute(Constants.CURRENT_USER);
+        if(user != null){
+            return ServerResponse.createBySeccessData(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取当前登录信息");
+    }
+
+}
