5663e87d554408bed85a6a4e5b40efc99d3a3f3a
luna <yljloveliucan@126.com>
2018-09-02 17:44:38 +0800

    restful interface change

--- src/main/java/com/luna/mall/controller/ProductController.java
+++ src/main/java/com/luna/mall/controller/ProductController.java
@@ -4,11 +4,10 @@ import com.github.pagehelper.PageInfo;
 import com.luna.mall.common.ServerResponse;
 import com.luna.mall.serivce.ProductService;
 import com.luna.mall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/product/")
@@ -23,6 +22,12 @@ public class ProductController {
         return productService.getProductDetail(productId);
     }
 
+    @RequestMapping(value = "/{productId}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> detailRestful(@PathVariable Integer productId){
+        return productService.getProductDetail(productId);
+    }
+
     @RequestMapping("list.do")
     @ResponseBody
     public ServerResponse<PageInfo> list(@RequestParam(value = "keyword", required = false)String keyword,
@@ -31,5 +36,60 @@ public class ProductController {
                                          @RequestParam(value = "pageSize", defaultValue = "10")int pageSize,
                                          @RequestParam(value = "orderBy", defaultValue = "")String orderBy){
         return productService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
+    }
+
+    @RequestMapping(value = "/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRestful(@PathVariable(value = "keyword")String keyword,
+                                         @PathVariable(value = "categoryId")Integer categoryId,
+                                         @PathVariable(value = "pageNum")Integer pageNum,
+                                         @PathVariable(value = "pageSize")Integer pageSize,
+                                         @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 20;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return productService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
+    }
+
+    @RequestMapping(value = "/keyword/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRestful(@PathVariable(value = "keyword")String keyword,
+                                         @PathVariable(value = "pageNum")Integer pageNum,
+                                         @PathVariable(value = "pageSize")Integer pageSize,
+                                         @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 20;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return productService.getProductByKeywordCategory(keyword, null, pageNum, pageSize, orderBy);
+    }
+
+    @RequestMapping(value = "/categoryId/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRestful(@PathVariable(value = "categoryId")Integer categoryId,
+                                         @PathVariable(value = "pageNum")Integer pageNum,
+                                         @PathVariable(value = "pageSize")Integer pageSize,
+                                         @PathVariable(value = "orderBy")String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize == null){
+            pageSize = 20;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return productService.getProductByKeywordCategory("", categoryId, pageNum, pageSize, orderBy);
     }
 }
